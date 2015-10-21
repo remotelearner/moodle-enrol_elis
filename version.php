06d9683534598424a9b4119ef144ff3375bdd18d
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-10-21 12:43:21 -0400

    ELIS-9250: Update versions releases and build dates.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015051100;
-$plugin->release = '2.9.0.0 (Build: 20150511)';
+$plugin->release = '2.9.2.1 (Build: 20151021)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2015051100.00;
