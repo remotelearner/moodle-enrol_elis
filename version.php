648c97b78db0e80cb4c37f3a48f8350393fa0a5d
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-09-19 16:33:08 -0400

    ELIS-9429: Update versions releases.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014082503;
-$plugin->release = '2.7.15.0 (Build: 20160815)';
+$plugin->release = '2.7.15.0 (Build: 20160919)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014051200.00;
